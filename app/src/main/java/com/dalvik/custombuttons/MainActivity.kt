da8b56de3c815c4b395b07e2e6cb2ecec3c01084
emmanuelpacheco <eeph31@gmail.com>
2022-05-16 11:59:37 -0500

    Change variable name

--- app/src/main/java/com/dalvik/custombuttons/MainActivity.kt
+++ app/src/main/java/com/dalvik/custombuttons/MainActivity.kt
@@ -6,14 +6,13 @@ import android.widget.Toast
 import com.dalvik.custombutton.CustomButton
 
 class MainActivity : AppCompatActivity() {
-    private var buttonred: CustomButton? = null
+    private var buttonCustom: CustomButton? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        buttonred = findViewById(R.id.customButton)
+        buttonCustom = findViewById(R.id.customButton)
 
-        buttonred!!.setOnClickListener {
-            buttonred!!.setLoading(true)
+        buttonCustom!!.setOnClickListener {
             Toast.makeText(this, "button clicked", Toast.LENGTH_LONG).show()
         }
 
